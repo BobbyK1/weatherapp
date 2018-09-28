5c85c31c8629b020af6152ebc3b39c0cd65e3913
BobbyK1 <marbobkara@gmail.com>
2018-09-28 09:18:28 -0500

    Homepage Updates

--- app.js
+++ app.js
@@ -6,7 +6,24 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("home");
+    var query = "Chicago";
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + query.replace(/\s/g, '') + "&APPID=5e2c16c0df201bf0e29a3c7267bfe0c9&units=imperial";
+
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200) {
+            let weather = JSON.parse(body);
+            let errormes = `${weather.cod}`;
+
+            if(errormes === 404) {
+                res.render("no-city");
+            } else {
+                let temp = parseInt(`${weather.main.temp}`);
+                let message = temp + " °F";
+                res.render("home", {temp: message,
+                                    city: query});
+            }
+        }
+    });
 });
 
 app.get("/results", function(req, res){
@@ -21,7 +38,7 @@ app.get("/results", function(req, res){
             if(errormes === 404) {
                 res.render("no-city");
             } else {
-                let temp = `${weather.main.temp}`;
+                let temp = parseInt(`${weather.main.temp}`);
                 let message = temp + " °F";
 
                 res.render("results", {temp: message,
